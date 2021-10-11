aad4f8a5802f7b905d0e94e785d78cf2b90c9119
David Rodrigues <56417462+trasherdave@users.noreply.github.com>
2021-10-11 20:20:08 -0300

    Add files via upload

---
+++ src/main/java/com/manoelcampos/cepservice/WidenetService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.manoelcampos.cepservice;
+
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author David
+ */
+@Dependent
+public final class WidenetService extends AbstractCepService {
+
+    private static final String DOMINIO = "https://apps.widenet.com.br/";
+
+//URL Absoluta:https://ws.apicep.com/cep/[cepCode].[format]
+//Exemplo:https://ws.apicep.com/cep/06233-030.json
+    
+//URL Relativa:https://ws.apicep.com/cep.[format]?code=[cepCode]
+//Exemplo:https://ws.apicep.com/cep.json?code=06233-030
+    
+    public WidenetService() {
+        super(DOMINIO);
+    }
+
+    @Override
+    protected String buildPath(final String cep) {
+        return "busca-cep/api/cep.json?code=" + cep;
+    }
+
+}
